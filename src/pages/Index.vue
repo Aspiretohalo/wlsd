3f8fd68a30f9e091942dd6ee4dfb55dee0024459
zjtter <131750293+zjtter@users.noreply.github.com>
2024-02-20 12:29:59 +0800

    答题

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,6 +5,7 @@
                 <TopNav></TopNav>
             </el-header>
             <el-main class="bgc" style="padding: 0;width: 100vw;overflow: hidden;">
+                <SideBar></SideBar>
                 <IndexMain></IndexMain>
             </el-main>
             <el-footer style="padding: 0;">
@@ -18,11 +19,14 @@
 import TopNav from '../components/TopNav.vue'
 import Bottom from '../components/Bottom.vue'
 import IndexMain from '../components/IndexMain.vue'
+import SideBar from '../components/SideBar.vue'
+
 import getUserMsg from '../functions/getUserMsg';
 import getAllExpert from '../functions/getAllExpert';
 import getAllCommittee from '../functions/getAllCommittee';
 import getAllWonderfulPictures from '../functions/getAllWonderfulPictures';
 import getAllMediaVideo from '../functions/getAllMediaVideo';
+
 import { onMounted } from 'vue'
 
 onMounted(async () => {
